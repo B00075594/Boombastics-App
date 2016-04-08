e3bd83da50a497d6002c11649b5f58b8259eb7f6
Darren Cosgrave <darrencosgrave101@gmail.com>
2016-04-08 15:03:25 +0100

    Added in our bio's and added in css to help ajust for mobile view

--- aboutNOTLOGIN.php
+++ aboutNOTLOGIN.php
@@ -15,17 +15,73 @@
 	<script src="javaScript/bootstrap.min.js" type="text/javascript"></script>
 	<style> @import "css/style.css";</style>
 </head>
-<body>
+<body class="special_layout">
 
     <?php include 'include/header.php';?>
 
-<center style="margin-top: 90px;">
-<h1>About</h1>
-<p>This app was developed by students of ITB.<br>
-Rachel Egan (CEO)<br>
-Ryan Mackenxie (CTO)<br>
-Darren Cosgrave (CGO)</p>
+<center style="margin-top: 50px;">
+	<div class="container">
+		<h1>About</h1>
 
+		<p>
+			Boombastics was established in July 2015, in a small caf&#233; in the quaint village of Baile Bhlains&#233;ir, Dublin
+			Ireland. It was founded as a small, family business by Rachel Egan Esq. BSc (almost), but quickly expanded. The company
+			has established independent Programming, Graphics and Testing departments. Each member of the team have been educated and
+			mentored by the top educators in the country.
+		</p>
+
+		<div>
+			<div class="p_left">
+				<img width="161" height="161" src="images/people/ryan.jpg" class="right" alt="pic of member ryan"/>
+
+				<h2>Ryan Mackenzie CTO</h2>
+				<p>
+					Ryan is a programming wizard (or so he thinks) who has an abundant supply of enthusiasm. He was a valuable member of the team once he learned to calm down
+					a bit. He vision for the project was complimented by his willingness to negotiate on a daily basis. Boombastics love him.
+				</p>
+			</div>
+		</div>
+
+		<div>
+			<div class="p_right">
+				<img width="161" height="161" src="images/people/cid.jpg" class="left" alt="pic of member cid" />
+
+				<h2>Alcidemar Lopes TTO</h2>
+				<p>
+				This smokey little genius is only a temporary addition to the country. Originally from Brazil, Alcidemar was a late-comer to the project. After extensive
+				interviews and police checks, we invited him to join Boombastics to enforce a stringent testing regime. Well, that was the plan. He contributed to the main
+				program on several occasions but he was able to refocus on his own department. Ireland and Boombastics love him!!
+				</p>
+			</div>
+		</div>
+
+		<div>
+			<div class="p_left">
+				<img width="161" height="200" src="images/people/darren.jpg" class="right" alt="pic of member darren"/>
+
+				<h2>Darren Cosgrave CGO</h2>
+				<p>
+				Darren is a mysterious creature. Hard to pin down, but once captured, is a diligent and hard-working beast. He brought a lot to this project including the
+				art of challenging the code, and button manipulation. With a keen eye for graphics (ehem!), Darren brought a lot to the GUI design and implementation
+				whenever he was around. Boombastics love him.
+				</p>
+			</div>
+		</div>
+
+		<div>
+
+			<div class="p_right">
+				<img width="161" height="161" src="images/people/rachel.jpg" class="left" alt="pic of member rachel"/>
+
+				<h2>Rachel Egan CPO</h2>
+				<p>
+					Rachel brought a skill set similar to that of a famous German dictator, whose name escapes me at this time, overseeing project deadlines and being sure to
+					help keep the team on track. Rachel's creative side came into play when designing the application in order for the best user experience. Rachel's constant
+					blabbering of all the research she conducted aided greatly in not only the coding side of the project but the written also. Boombastics HATE her.
+				</p>
+			</div>
+		</div>
+	</div>
 </center>
 </body>
 </html>

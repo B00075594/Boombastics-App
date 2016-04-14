8ebaf1332f143d98986a3bdf1a7499da170b2ab4
Darren Cosgrave <darrencosgrave101@gmail.com>
2016-04-14 15:56:04 +0100

    Minor css shading

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,3 @@
-<footer>
+<footer style="margin-top: 15px;">
    <p style="font-size:13px">&copy 2016  <a style="font-size:13px" href="docs/termsandconditions.pdf">T&C</a>   <a style="font-size:13px" href="docs/privacypolicy.pdf">Privacy Policy</a></p>
 </footer>
